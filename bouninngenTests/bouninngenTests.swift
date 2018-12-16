328744a8b45f32e405949618c6df8cb7c7c99a95
杉村和栄 <sugimurakazue@macbook-air.local>
2018-12-16 11:51:08 +0900

    Initial Commit

---
+++ bouninngenTests/bouninngenTests.swift
@@ -0,0 +1,34 @@
+//
+//  bouninngenTests.swift
+//  bouninngenTests
+//
+//  Created by 杉村和栄 on 2018/12/16.
+//  Copyright © 2018年 杉村和栄. All rights reserved.
+//
+
+import XCTest
+@testable import bouninngen
+
+class bouninngenTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
